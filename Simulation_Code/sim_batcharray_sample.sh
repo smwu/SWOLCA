498afab6ae82047ae703fce9e3700a10d064151f
Stephanie M Wu <swu@g.harvard.edu>
2022-03-02 09:18:37 -0500

    Modified simulation code

--- Simulation_Code/sim_batcharray_sample.sh
+++ Simulation_Code/sim_batcharray_sample.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-#SBATCH -J sim_SRS 	      # Job name
-#SBATCH -o sim_SRS_%A.out  # Shared std out for job array
+#SBATCH -J sim_strat_eq 	      # Job name
+#SBATCH -o sim_strat_eq_%A.out  # Shared std out for job array
 #SBATCH -p shared             # Partition
 #SBATCH -n 1                  # Number of nodes
 #SBATCH --mem 1000             # Memory request in MB
@@ -10,6 +10,7 @@
 
 module load matlab
 samp_n=1
-for scenario in $(seq 1 4); do
+for scenario in $(seq 17 18); do
 	echo This is population scenario ${scenario} iteration ${SLURM_ARRAY_TASK_ID} sample ${samp_n}.$'\n'
-matlab -nodisplay -nosplash -r "sample_SRS(${scenario},${SLURM_ARRAY_TASK_ID},${samp_n})" 
+matlab -nodisplay -nosplash -r "sample_strat_eq(${scenario},${SLURM_ARRAY_TASK_ID},${samp_n})"
+done 
